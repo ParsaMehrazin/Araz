7ae3e605efc8e466860232ca2bd75ce4834749af
parsa.mehrazin123@gmail.com <parsa.mehrazin123@gmail.com>
2024-08-19 12:09:45 +0330

    #fix Error

--- Araz/Araz_Utilities/Utilities.cs
+++ Araz/Araz_Utilities/Utilities.cs
@@ -1,32 +1,15 @@
 ﻿//using DevExpress.CodeParser;
 using DevExpress.XtraEditors;
 using DevExpress.XtraSplashScreen;
-
 using System.Diagnostics;
-using ViewModel.ViewModels;
+
 
 
 
 namespace Araz_Utilities
 {
     public class Utilities
-    {
-        public class CommonTools
-        {
-            public static void Loading(bool start = false)
-            {
-                if (start)
-                {
-                    if (SplashScreenManager.Default == null || !SplashScreenManager.Default.IsSplashFormVisible)
-                        SplashScreenManager.ShowForm(typeof(WaitingForm));
-                }
-                else
-                {
-                    if (SplashScreenManager.Default != null && SplashScreenManager.Default.IsSplashFormVisible)
-                        SplashScreenManager.CloseForm();
-                }
-            }
-        }
+    {    
 
 
 
